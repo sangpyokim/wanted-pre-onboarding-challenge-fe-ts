fb03e8fd7460ff8db810bbd3127d6e30b7a21648
sangpyokim <rlatkdvy12@naver.com>
2023-02-10 16:39:47 +0900

    first commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# [Pre Onboarding] 3차 과제 김상표
+
+## 1. 이새미, 코드 바로가기 [설계](https://github.com/shinpanda/wanted-pre-onboarding-challenge-fe-2/blob/master/index.ts) |  [구현](https://github.com/shinpanda/wanted-pre-onboarding-challenge-fe-2/blob/master/index.ts)
+
+### 피드백
+
+- TTodos:
+  -  read: 오버로드 해야함 ex) read(): iTodo[], read(id: number): iTodo | iTodo[]
+  -  delete: 메소드 분리말고 마찬가지로 오버로드
+  
+
+| 설계 코드      | 구현 코드                                        |
+| ------------ | -------------------------------------------------- |
+| <img  title="이새미 설계 코드" width="450px" src="https://firebasestorage.googleapis.com/v0/b/classmate-e.appspot.com/o/pre%20onboarding%203%2F1%20(1).png?alt=media&token=6b9a30bd-f705-43c1-9b47-9a556b89c9aa" /> | <img  title="이새미 설계 코드" width="450px" src="https://firebasestorage.googleapis.com/v0/b/classmate-e.appspot.com/o/pre%20onboarding%203%2Fcarbon%20(2)%20(1).png?alt=media&token=a682e00e-28eb-4674-b538-4c497895b5ea" />  |
---
+++ 이새미/index.ts
@@ -0,0 +1,96 @@
+// 설계
+interface iTodo {
+  id: number;
+  content: string;
+  isCompleted: boolean;
+  category: string;
+  tags?: string[];
+}
+
+// class TTodos {
+//   private ids: iTodo[];
+//   create(
+//     content: string,
+//     isCompleted: boolean,
+//     category: string,
+//     tags?: string[]
+//   ): number {
+//     return 0;
+//   }
+
+//   read(id: number): iTodo | iTodo[] {
+//     return [];
+//   }
+
+//   delete(id: number) {}
+
+//   deleteTag(id: number, deleteTag: string[]) {}
+// }
+
+// 구현
+// 태그는 필수
+class Todo implements iTodo {
+  id: number;
+  content: string;
+  isCompleted: boolean;
+  category: string;
+  tags?: string[];
+
+  constructor(id: number, content: string, isCompleted: boolean, category: string, tags: string[] = []) {
+    this.id = id
+    this.content = content
+    this.category = category
+    this.isCompleted = isCompleted
+    this.tags = tags
+  }
+}
+
+class TTodos {
+  private ids: iTodo[];
+
+  create(
+    content: string,
+    isCompleted: boolean,
+    category: string,
+    tags?: string[]
+  ): number {
+    if (content.length === 0) return -1
+
+    const newTodo = new Todo(new Date().getTime(), content, isCompleted, category, tags)
+    this.ids.push(newTodo)
+
+    return 0;
+  }
+
+  read(id: number): iTodo | iTodo[] {
+    return this.ids.find(ele => ele.id === id) || []
+  }
+
+  delete(id: number) {
+    const findTodoIndex = this.ids.findIndex(ele => ele.id === id)
+    this.ids.splice(findTodoIndex, 1)
+  }
+
+  deleteTag(id: number, deleteTag: string[]) {
+    this.delete(id)
+
+    const tagSet = new Set(deleteTag)
+    const result = this.filterArrByTag(this.ids, tagSet)
+
+    this.ids = result
+  }
+
+  private filterArrByTag(arr: iTodo[], tagSet: Set<string>) {
+    const temp = structuredClone(arr)
+
+    const res = temp.filter(todo => {
+      if (!todo.tags) return true
+
+      for (let tag of todo.tags) {
+        if (tagSet.has(tag)) return false
+      }
+    });
+
+    return res
+  }
+}
